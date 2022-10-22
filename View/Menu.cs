945b238a5b84092738ca6971226723117e0ae7c7
Dinal Atapattu <dinalatapattu@Dinals-MacBook-Pro.local>
2022-10-22 23:28:15 +1000

    backup

--- View/Menu.cs
+++ View/Menu.cs
@@ -1,24 +1,30 @@
 using static System.Console;
 using Auction.Model;
 
-namespace Auction.View {
-    public class Menu : Dialog {
+namespace Auction.View
+{
+    public class Menu : Dialog
+    {
         const string HEADING = $"Please select an option between 1 and {opt.Length}";
         // const string TITLE = $"+{new string('-',30)}+ \n| Welcome to the Auction House |\n+{new string('-',30)}+";
         const string PROMPT = "> ";
-        private IDisplay[] opt;
-        public Menu(string title,ProductStorage products, ClientStorage clients, params IDisplay[] options) : base(title, products, clients) {
+        private IDisplay[] options;
+        public Menu(string title, ProductStorage products, ClientStorage clients, params IDisplay[] options) : base(title, products, clients)
+        {
             this.options = new IDisplay[options.Length];
         }
-        public void Options() {
+        public void ShowOptions()
+        {
             WriteLine();
             WriteLine(TITLE);
-            WriteLine(new string('-',Title.Length));
-            foreach (var (option, index) in options) {
+            WriteLine(new string('-', Title.Length));
+            foreach (var (option, index) in options)
+            {
                 WriteLine($"({index}) {option.Title}");
             }
             WriteLine();
             WriteLine(HEADING);
         }
+        public bool ValidOption (out IDisplay opt) {}
     }
 }
